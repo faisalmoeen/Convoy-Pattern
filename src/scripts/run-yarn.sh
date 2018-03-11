cfe99a48e9b9a36947a3ccc6412441463ab0a347
faisalmoeen <fmorakzai@gmail.com>
2018-03-11 19:48:50 +0100

    run-yarn.sh for DCM MR

--- src/scripts/run-yarn.sh
+++ src/scripts/run-yarn.sh
@@ -2,99 +2,50 @@
 
 
 DATASETS=(trucks-d brink-d tdrive-d)
-trucks_time_offsets={2 2875}
-tdrive_time_offsets={0 53328}
-brink_time_offsets={2 20000}
-CLUSTERING_EXP_RESULT=clustering_exps.txt
-SPARE_EXP_RESULT=spare_exps.txt
+declare -A time_offsets
+time_offsets[trucks-d_start]=2
+time_offsets[trucks-d_end]=2875
+time_offsets[brink-d_start]=2
+time_offsets[brink-d_end]=20000
+time_offsets[tdrive-d_start]=0
+time_offsets[tdrive-d_end]=53328
+DCM_MR_EXP_RESULT=dcm_mr_exps.txt
 CORES=(16 14 12 10 8 6 4 2)
-CLUSTERING_IN_DIR=/user/faisal/datasets
-CLUSTERING_OUT_DIR=/user/faisal/output
+IN_DIR=/user/faisal/datasets
+OUT_DIR=/user/faisal/output
 
 
-#===============================================Clustering Phase============================================
+#===============================================DCM_MR============================================
 
 E=(0.000060) #eps for DBSCAN
 M=(6) #min num of objs in a cluster
-H=(87) #hdfs partitions
-S=(87) #snapshot partitions
-r=0 #use earth distance calc = false
+K=(180)
 
 
-rm ${CLUSTERING_EXP_RESULT}
-touch ${CLUSTERING_EXP_RESULT}
-echo "dataset,e,m,cores,time(ms)" >> ${CLUSTERING_EXP_RESULT}
+rm ${DCM_MR_EXP_RESULT}
+touch ${DCM_MR_EXP_RESULT}
+echo "dataset,e,m,cores,time(ms)" >> ${DCM_MR_EXP_RESULT}
 
 for dataset in ${DATASETS[@]}
-do
-for core in ${CORES[@]}
 do
 for m in ${M[@]}
 do
 for e in ${E[@]}
-do
-    start=$(date +%s%3N)
-    echo $dataset,${e},${m}
-    hdfs dfs -rm -r ${CLUSTERING_OUT_DIR}/${dataset}/clusters-e${e}-m${m}
-    spark-submit --master yarn --deploy-mode client --executor-cores ${core} \
-        jars/TrajectoryMining-0.0.1-SNAPSHOT.jar input.MainApp e=${e} m=${m} \
-        input=${CLUSTERING_IN_DIR}/${dataset} output=${CLUSTERING_OUT_DIR}/${dataset}
-    end=$(date +%s%3N)
-    echo $dataset,${e},${m},${core},$(($end-$start))
-    echo "$dataset,${e},${m},${core},$(($end-$start))" >> ${CLUSTERING_EXP_RESULT}
-done
-done
-done
-done
-
-# ================================Trajectory Mining==============================
-
-
-rm ${SPARE_EXP_RESULT}
-touch ${SPARE_EXP_RESULT}
-echo "dataset,k,l,m,g,c,cores,time(ms)" >> $SPARE_EXP_RESULT
-
-CORES=(1 2 3 4 5 6 7 8)
-#K=(90 180 270 360)
-K=(180)
-L=(180) #length of local consecutive pattern
-M=(6)
-G=(0) # gap
-C=(115) # Num of clique miner partitions
-
-
-for dataset in ${DATASETS[@]}
-do
-for c in ${C[@]}
 do
 for k in ${K[@]}
 do
-for l in ${L[@]}
-do
-for m in ${M[@]}
-do
-for e in ${E[@]}
-do
-for g in ${G[@]}
-do
-for core in ${CORES[@]}
-do
     start=$(date +%s%3N)
-    echo ${dataset},${k},${l},${m},${g},${c}
-    spark-submit --master yarn --deploy-mode client --executor-cores ${core} \
-        jars/TrajectoryMining-0.0.1-SNAPSHOT.jar apriori.MainSP k=${k} l=${l} m=${m} g=${g} c=${c}\
-        input=${CLUSTERING_OUT_DIR}/${dataset}/clusters-e${e}-m${m}
+    echo $dataset,${e},${m},${k}
+    hadoop jar target/DistributedConvoy-0.0.1-SNAPSHOT.jar mapreduce.ConvoyJobNlognMRSplits ${IN_DIR}/${dataset} ${OUT_DIR}/${dataset} \
+    ${m} ${k} ${e} ${time_offsets[${dataset}_start]} ${time_offsets[${dataset}_end]}
     end=$(date +%s%3N)
-    echo ${dataset},${k},${l},${m},${g},${c},$(($end-$start))
-    echo "${dataset},${k},${l},${m},${g},${c},${core},$(($end-$start))" >> $SPARE_EXP_RESULT
-done
-done
-done
-done
+    echo $dataset,${e},${m},${k},${core},$(($end-$start))
+    echo "$dataset,${e},${m},${k},${core},$(($end-$start))" >> ${DCM_MR_EXP_RESULT}
 done
 done
 done
 done
 
 
-java -cp target/DistributedConvoy-0.0.1-SNAPSHOT.jar mapreduce.ConvoyJobNlognMRSplits /user/faisal/datasets/trucks-d /user/faisal/output/dcm-mr/trucks-d 6 180 0.00006
+java -cp target/DistributedConvoy-0.0.1-SNAPSHOT.jar mapreduce.ConvoyJobNlognMRSplits /user/faisal/datasets/trucks-d /user/faisal/output/dcm-mr/trucks-d
+hadoop jar target/DistributedConvoy-0.0.1-SNAPSHOT.jar mapreduce.ConvoyJobNlognMRSplits /user/faisal/datasets/trucks-d /user/faisal/output/trucks-d/DCM 6 180 0.00006 2 2875
